166ac9a8b96197686d2ce017781dec9948436c67
BashFromOhio <bashholt@gmail.com>
2024-01-30 13:09:18 +0530

    Updated package and crud

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
-__version__ = "0.0.4"
+__version__ = "0.0.10 "
 REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "BashFromOhio"
